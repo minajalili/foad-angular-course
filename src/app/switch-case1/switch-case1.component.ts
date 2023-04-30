dce6d2cc6e044b15fb48d071c1438a64cc97b254
mina.ja <mina.ja@abramad.com>
2023-04-30 16:57:05 +0330

    services part 1

--- src/app/switch-case1/switch-case1.component.ts
+++ src/app/switch-case1/switch-case1.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-switch-case1',
   templateUrl: './switch-case1.component.html',
-  styleUrls: ['./switch-case1.component.css']
+  styleUrls: ['./switch-case1.component.css'],
 })
-export class SwitchCase1Component {
+export class SwitchCase1Component implements OnInit {
+  constructor(private loggingService: LoggingService) {}
 
+  ngOnInit(): void {
+    this.loggingService.log('Hi From App-switch-case1');
+  }
+
+  public log() {
+    this.loggingService.log('Hi From App-switch-case1');
+  }
 }
